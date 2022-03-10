57a49bd71b7730e5bae29381c0d3b423aed53a05
Oleg P <olegp.msn@gmail.com>
2022-03-10 16:40:38 +0200

    bbc

--- bbccom/bbccom/spiders/bbc.py
+++ bbccom/bbccom/spiders/bbc.py
@@ -1,18 +1,63 @@
 import scrapy
 
+
 class BbcSpider(scrapy.Spider):
     name = 'bbc'
     allowed_domains = ['www.bbc.com']
     start_urls = ['http://www.bbc.com/']
 
     def parse(self, response):
-        bbclink = response.xpath("//a[@class='block-link__overlay-link']/@href").getall()
-        yield from response.follow_all(bbclink, self.parsePage)
+        bbc_link = response.xpath("//a[@class='block-link__overlay-link']/@href").getall()
+        yield from response.follow_all(bbc_link, self.parsePage)
 
     def parsePage(self, response):
+        site_url = response.request.url
+
+        title = response.xpath("//article/header/h1/text()").get()
+
+        if "/sport/live/" in site_url:
+            title = None#"/sport/live/"
+
+        if "/news/live/" in site_url:
+            title = None# "/news/live/"
+
+        if "/future/article/" in site_url:
+            title = None#"/future/article/"
+
+        if "/travel/article/" in site_url:
+            title = None#"/travel/article/"
+            title = response.xpath(
+                "//section/div[@class='article-headline__text b-reith-sans-font b-font-weight-300]'/text()").get()
+
+        if "/culture/article/" in site_url:
+            title = "/culture/article/"
+            title = response.xpath(
+                "//section/div[@class='article-headline__text b-reith-sans-font b-font-weight-300]'/text()").get()
+
+        if "/news/av/" in site_url:
+            title = "/news/av/"
+            title = response.xpath("//article/div/h1/text()").get()
+
+        if "/sport/av/" in site_url:
+            title = "/sport/av/"
+            title = response.xpath("//article/div/h1/text()").get()
+
+        if "/worklife/article/" in site_url:
+            title = "/worklife/article/"
+
+    #    if (title is None) or (title is ""):
+    #        title = response.xpath("//article/div/h1/text()").get()
+
+
+        #description = response.xpath('//article/div[@data-component="text-block"]/div/p/text()').getall()
+
+    #    if (description is None) or (description is ""):
+    #        response.xpath('//article/div[@aria-live="polite"]/div').getall()
+
         for page in response.xpath("//article"):
             yield {
-
-                'title': page.xpath("//article/div/h1[@id='main-heading']/text()").get(),
-                'description':  page.xpath("//article").extract()
+                'url': site_url,
+                'title': title,
+                #'description': description
             }
+
--- bbccom/bbccom/spiders/page.py
+++ bbccom/bbccom/spiders/page.py
@@ -1,6 +1,7 @@
 import scrapy
 
-class BbcSpider(scrapy.Spider):
+
+class BbcPageSpider(scrapy.Spider):
     name = 'bbc_page'
     allowed_domains = ['www.bbc.com']
     start_urls = ['https://www.bbc.com/news/world-europe-60675599']
@@ -8,7 +9,6 @@ class BbcSpider(scrapy.Spider):
     def parse(self, response):
         for page in response.xpath("//article"):
             yield {
-                'title': page.xpath("//article/div/h1[@id='main-heading']/text()").get(),
-                'description':  page.xpath("//article").extract()
+                'title': page.xpath("//article/header/h1/text()").get(),
+                'description':  page.xpath('//article/div[@data-component="text-block"]/div/p/text()').getall()
             }
-
