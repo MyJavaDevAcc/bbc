b90e0b090676f17359e4ab2c181371fac138916b
Oleg P <olegp.msn@gmail.com>
2022-03-12 11:58:37 +0200

    bbc

--- bbccom/bbccom/spiders/bbc.py
+++ bbccom/bbccom/spiders/bbc.py
@@ -14,50 +14,54 @@ class BbcSpider(scrapy.Spider):
         site_url = response.request.url
 
         title = response.xpath("//article/header/h1/text()").get()
+        description = response.xpath('//article/div[@data-component="text-block"]/div/p/text()').getall()
 
         if "/sport/live/" in site_url:
-            title = None#"/sport/live/"
+            title = None
+            description = ""
 
         if "/news/live/" in site_url:
-            title = None# "/news/live/"
+            title = None
+            description = ""
 
         if "/future/article/" in site_url:
-            title = None#"/future/article/"
-
-        if "/travel/article/" in site_url:
-            title = None#"/travel/article/"
-            title = response.xpath(
-                "//section/div[@class='article-headline__text b-reith-sans-font b-font-weight-300]'/text()").get()
-
-        if "/culture/article/" in site_url:
-            title = "/culture/article/"
-            title = response.xpath(
-                "//section/div[@class='article-headline__text b-reith-sans-font b-font-weight-300]'/text()").get()
-
-        if "/news/av/" in site_url:
-            title = "/news/av/"
-            title = response.xpath("//article/div/h1/text()").get()
-
-        if "/sport/av/" in site_url:
-            title = "/sport/av/"
-            title = response.xpath("//article/div/h1/text()").get()
+            title = None
+            description = ""
 
         if "/worklife/article/" in site_url:
-            title = "/worklife/article/"
+            title = response.xpath(
+                "//section/div[@class='article-headline__text b-reith-sans-font b-font-weight-300]'/text()").get()
+            description = ""
+
+        if "/travel/article/" in site_url:
+            title = response.xpath(
+                "//section/div[@class='article-headline__text b-reith-sans-font b-font-weight-300]'/text()").get()
+            description = response.xpath('//article/div[@data-component="text-block"]/div/p/text()').getall()
+
+        if "/culture/article/" in site_url:
+            title = response.xpath(
+                "//section/div[@class='article-headline__text b-reith-sans-font b-font-weight-300]'/text()").get()
+            description = response.xpath('//article/div[@data-component="text-block"]/div/p/text()').getall()
+
+        if "/news/av/" in site_url:
+            title = response.xpath("//article/div/h1/text()").get()
+            response.xpath('//article/div[@aria-live="polite"]/div').getall()
+
+        if "/sport/av/" in site_url:
+            title = response.xpath("//article/div/h1/text()").get()
+            description = response.xpath('//article/div[@data-component="text-block"]/div/p/text()').getall()
 
     #    if (title is None) or (title is ""):
     #        title = response.xpath("//article/div/h1/text()").get()
-
-
-        #description = response.xpath('//article/div[@data-component="text-block"]/div/p/text()').getall()
-
+    #    description = response.xpath('//article/div[@data-component="text-block"]/div/p/text()').getall()
     #    if (description is None) or (description is ""):
     #        response.xpath('//article/div[@aria-live="polite"]/div').getall()
 
         for page in response.xpath("//article"):
-            yield {
-                'url': site_url,
-                'title': title,
-                #'description': description
-            }
+            if title:
+                yield {
+                    'url': site_url,
+                    'title': title,
+                    'description': description
+                }
 
